1a56dbcfe52de0d6e81908a1cd3799d1ff8e9453
Kyrllan Nogueira de Souza <kyrllan@hotmail.com>
2025-11-26 10:47:19 -0300

    pwa support added

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      title: 'Cash Pilot',
+      meta: [
+        { name: 'description', content: 'Cash Pilot is a cash management tool that helps you keep track of your cash flow.' }
+      ],
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      ]
+    }
+  },
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint', '@nuxt/ui', '@nuxtjs/supabase'],
+  modules: ['@nuxt/eslint', '@nuxt/ui', '@nuxtjs/supabase', '@vite-pwa/nuxt'],
   css: ['~/assets/css/main.css'],
   supabase: {
     redirectOptions: {
@@ -16,6 +27,35 @@ export default defineNuxtConfig({
   public: {
     supabaseUrl: process.env.NUXT_PUBLIC_SUPABASE_URL,
     supabaseKey: process.env.NUXT_PUBLIC_SUPABASE_KEY
+    }
+  },
+  pwa: {
+    manifest: {
+      name: 'Cash Pilot',
+      short_name: 'Cash Pilot',
+      theme_color: '#02DF72',
+      background_color: '#ffffff',
+      display: 'standalone',
+      start_url: '/',
+      icons: [
+        {
+          src: '/icons/pwa-192x192.png',
+          sizes: '192x192',
+          type: 'image/png'
+        },
+        {
+          src: '/icons/pwa-512x512.png',
+          sizes: '512x512',
+          type: 'image/png'
+        }
+      ]
+    },
+
+    registerType: 'autoUpdate',
+
+    workbox: {
+      navigateFallback: '/',
+      globPatterns: ['**/*.{js,css,html,png,svg,ico,json}']
     }
   }
 })
